604ce0158bfb4a67b3b558c95b0ab9baf2bba1cc
Victor Parizio <victorparizio@hotmail.com>
2023-03-11 09:36:30 -0300

    feat (02-server-exercicio) Exercício Url Foi ensinado como criar/personalizar url's e personalizar a resposta do server no caso de url desconhecida.

---
+++ 02-server-exercicio.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const host = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res)=>{
+    if(req.url == '/'){
+        res.end('Página inicial do nosso website')
+    } else if (req.url == '/new'){
+        res.end('Página nova do nosso website')
+    } else if (req.url == '/contacts'){
+        res.end('Página de contacts do nosso website')
+    } else {
+        res.end('Página desconhecida')
+    }
+})
+
+server.listen(port, host, ()=>{
+    console.log('Novo servidor em função.')
+})
