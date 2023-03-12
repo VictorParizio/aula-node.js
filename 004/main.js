9e4056fb7186047a255c909307e8185c4403a3f2
Victor Parizio <victorparizio@hotmail.com>
2023-03-12 07:45:40 -0300

    feat (main) Introdução aos Módulos Built in Nesta aula foi ensinado como ter acesso as informações do módulo "os" (Sistema Operacionaol) por meio das funções e objetos deste módulo.

---
+++ 004/main.js
@@ -0,0 +1,11 @@
+const os = require('os')
+
+// const.log(os)
+// console.log(os.platform())
+// console.log(os.version())
+// console.log(os.cpus())
+// console.log(os.homedir())
+// console.log(os.freemem())
+// console.log(os.userInfo())
+// console.log(os.machine())
+console.log(os.totalmem())
