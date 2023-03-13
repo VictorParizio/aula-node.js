2718599021879477dd875be1348a6fe9ad3f7994
Victor Parizio <victorparizio@hotmail.com>
2023-03-13 10:35:33 -0300

    feat (server) Relembrando conceitos Nessa aula vimos como analisar, tratar e construir respostas para o browser com base no require feito pelo cliente

---
+++ 008/server.js
@@ -0,0 +1,18 @@
+const http = require ('http')
+
+const server = http.createServer((req, res)=>{
+
+    //header
+    res.setHeader('Content-Type', 'text/html')
+
+    res.write('<head><link rel="stylesheet" href="app.css"></head>')
+    res.write('<h2>Título Principal</h2>')
+    res.write('<h4>Subtítulo</h4>')
+    res.write('<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, repudiandae ratione sed tempora eos facilis nostrum debitis nobis nemo voluptates iusto recusandae reprehenderit nam quasi maiores sunt, provident consectetur sequi.</p>')
+    
+    res.end()
+})
+
+server.listen(3000, 'localhost',()=>{
+    console.log('servidor iniciado')
+})
