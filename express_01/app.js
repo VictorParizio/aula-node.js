c922052e158b2ba1647023a9eb3f6e3ada71cf39
Victor Parizio <victorparizio@hotmail.com>
2023-03-16 10:26:51 -0300

    feat (app, inicio) Express - Template Engine (EJS) Nesta aula vimos como instalar e usar o Template Engine a partir do ejs. Para que a construção desta aplicação funcionasse de acordo com o script da aula, foi especificada a pasta onde se encontra o arquivo ejs por meio do "app.set(views, ./express_01/views)", que nos permite definir parametros da própria aplicação.

--- express_01/app.js
+++ express_01/app.js
@@ -3,27 +3,35 @@ const express = require('express')
 // cria uma aplicação express
 const app = express()
 
+// registrar o template engine (view engine)
+app.set('view engine', 'ejs')
+app.set('views', './express_01/views')
+
 // escuta os requests
 app.listen(3000)
 
 // routes
 app.get('/', (req, res) => {
-    res.status(200).sendFile('./views/home.html', { root: __dirname })
+    res.sendFile('./views/home.html', { root: __dirname })
 })
 
 app.get('/about', (req, res) => {
-    res.status(200).sendFile('./views/about.html', { root: __dirname })
+    res.sendFile('./views/about.html', { root: __dirname })
 })
 
 app.get('/services', (req, res) => {
-    res.status(200).sendFile('./views/services.html', { root: __dirname })
+    res.sendFile('./views/services.html', { root: __dirname })
 })
 
 app.get('/sobre', (req, res) => {
-    res.status(302).redirect('/about')
+    res.redirect('/about')
+})
+
+//ejs
+app.get('/teste-ejs', (req, res) => {
+    res.render('inicio')
 })
 
 app.use((req, res) => {
-    // res.sendFile('./views/404.html', { root: __dirname })
     res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
