567f4dd8a07547fa7c9b9b81b3d88a675963bb4d
Victor Parizio <victorparizio@hotmail.com>
2023-03-17 08:21:19 -0300

    feat (app, services) Express - Coleção de Resultados Nesta aula vimos como passar uma coleção de resultados, vindos de um servidor, para um "componente" e "renderizar" vários elementos na tela sem ter que ficar repetindo código, muito semelhante as aplicações React.js

--- express_01/app.js
+++ express_01/app.js
@@ -20,7 +20,14 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/services', (req, res) => {
-    res.render('services', { title: "Serviços" })
+
+    const servicos = [
+        { titulo_servico: "Desenvolvimento Web", descricao: "Desenvolvemos páginas e apps web." },
+        { titulo_servico: "Desenvolvimento Desktop", descricao: "Desenvolvemos aplicações Desktop." },
+        { titulo_servico: "Desenvolvimento Mobile", descricao: "Desenvolvemos apps mobile para Android e iOS" }
+    ]
+
+    res.render('services', { title: "Serviços", servicos })
 })
 
 app.use((req, res) => {
